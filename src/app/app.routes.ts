f5e7b24cc86826fce097ae1d0c54a2b23de120a6
Damcio Adamcio <damian.szpura@gmail.com>
2024-04-06 18:42:53 +0200

    tech - clenaup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const additionalRoutes: Routes = [
+    {
+        path: 'lazyloaded-module',
+        loadChildren: () => import('@modules').then(
+            (m) => m.LazyloadedModule
+        ),
+    },
+]
+
+export const routes: Routes = [
+    // From newer versions, Angular team sugest to actually use the standalone components, instead of modules.
+    {
+        path: 'lazyloaded-module',
+        loadChildren: () => import('@modules').then(
+            (m) => m.LazyloadedModule
+        ),
+    },
+    {
+        path: 'lazyloaded-module-with-dynamic-routes', 
+        loadChildren: () => import('@modules').then(
+            (m) => m.LazyloadedWithDynamicRoutesModule
+        ),
+        data: { additionalRoutes }
+    }
+];
